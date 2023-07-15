551038462c0271dd600e22bbeb120dfbfb1c8681
Cepik17 <cepik17@gmail.com>
2023-07-15 22:34:57 +0600

    App is ready

--- src/test/java/silkpay/silkpayTest/SilkpayTestApplicationTests.java
+++ src/test/java/silkpay/silkpayTest/SilkpayTestApplicationTests.java
@@ -1,9 +1,7 @@
 package silkpay.silkpayTest;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import silkpay.silkpayTest.services.AccountService;
 
 @SpringBootTest
 class SilkpayTestApplicationTests {
